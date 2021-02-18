95e9c1e5326a1ff17a1321f88354fb8f4a3d20f8
Isaac Oduh <ufuomaoduh@gmail.com>
2021-02-18 16:55:23 +0100

    Build out data serializers and mapper : Customer and Order

---
+++ Serialization/CustomerMapper.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MartPortDev.Data;
+using MartPortDev.ViewModels;
+
+namespace MartPortDev.Serialization
+{
+    public class CustomerMapper
+    {
+        /// <summary>
+        /// Serializes a Customer data model into a CustomerModel view model
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+        /// 
+        public static CustomerModel SerializeCustomer(Customer customer)
+        {
+            return new CustomerModel
+            {
+                Id = customer.Id,
+                CreatedAt = customer.CreatedAt,
+                UpdatedAt = customer.UpdatedAt,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+
+                CustomerDetail = MapCustomerDetail(customer.customerDetails),
+            };
+        }
+
+        /// <summary>
+        /// Serializes a CustomerModel view model into a Customer data model
+        /// </summary>
+        /// <param name="customer"></param>
+        /// <returns></returns>
+        /// 
+        public static Customer SerializeCustomer(CustomerModel customer)
+        {
+            return new Customer
+            {
+                CreatedAt = customer.CreatedAt,
+                UpdatedAt = customer.UpdatedAt,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                customerDetails = MapCustomerDetail(customer.CustomerDetail),
+            };
+        }
+
+        /// <summary>
+        /// Maps a CustomerDetail data model to a CustomerDetailModel view model
+        /// </summary>
+        /// <param name="detail"></param>
+        /// <returns></returns>
+        /// 
+        public static CustomerDetailModel MapCustomerDetail(CustomerDetail detail)
+        {
+            return new CustomerDetailModel
+            {
+                Id = detail.Id,
+                Email = detail.Email,
+                Phone = detail.Phone,
+                PrimaryAddress = detail.PrimaryAddress,
+                AlternateAddress = detail.AlternateAddress,
+                City = detail.City,
+                State = detail.State,
+                PostalCode = detail.PostalCode,
+                Country = detail.Country,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+            };
+        }
+
+        public static CustomerDetail MapCustomerDetail(CustomerDetailModel detail)
+        {
+            return new CustomerDetail
+            {
+                Email = detail.Email,
+                Phone = detail.Phone,
+                PrimaryAddress = detail.PrimaryAddress,
+                AlternateAddress = detail.AlternateAddress,
+                City = detail.City,
+                State = detail.State,
+                PostalCode = detail.PostalCode,
+                Country = detail.Country,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow,
+            };
+        }
+    }
+}
---
+++ Serialization/OrderMapper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MartPortDev.Data;
+using MartPortDev.ViewModels;
+
+namespace MartPortDev.Serialization
+{
+    public class OrderMapper
+    {
+        public static SalesOrder SerializeInvoiceToOrder(InvoiceModel invoice)
+        {
+            var salesOrderItems = invoice.LineItems.Select(item => new SalesOrderItem
+            {
+                Id = item.Id,
+                Quantity = item.Quantity,
+                Material = MaterialMapper.SerializeMaterialModel(item.Material)
+            }).ToList();
+
+            return new SalesOrder
+            {
+                SalesOrderItems = salesOrderItems,
+                CreatedAt = DateTime.UtcNow,
+                UpdatedAt = DateTime.UtcNow
+            };
+        }
+
+        public static List<OrderModel> SerializeOrderToViewModels(IEnumerable<SalesOrder> orders)
+        {
+            return orders.Select(order => new OrderModel
+            {
+                Id = order.Id,
+                UpdatedAt = order.UpdatedAt,
+                CreatedAt = order.CreatedAt,
+                SalesOrderItems = SerializeSalesOrderItems(order.SalesOrderItems),
+                Customer = CustomerMapper.SerializeCustomer(order.Customer),
+                IsPaid = order.IsPaid
+            }).ToList();
+        }
+
+        private static List<SalesOrderItemModel> SerializeSalesOrderItems(IEnumerable<SalesOrderItem> orderItems)
+        {
+            return orderItems.Select(item => new SalesOrderItemModel
+            {
+                Id = item.Id,
+                Quantity = item.Quantity,
+                Material = MaterialMapper.SerializeMaterialModel(item.Material)
+            }).ToList();
+        }
+    }
+}
